f4a49ae38302751e891b84309622d4ba67dfd8bd
Anton Krivelo <krivelo2017@mail.ru>
2025-08-07 12:25:17 +0300

    install project ts

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,11 +1,37 @@
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+import 'leaflet/dist/leaflet.css';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
+  const position: [number, number] = [55.751244, 37.618423];
+
+  delete (L.Icon.Default.prototype as any)._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
   return (
     <div className="App">
-      <header className="App-header">
-        123
-      </header>
+      <MapContainer
+      center={position}
+      zoom={13}
+      style={{ height: '100vh', width: '100%' }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+      />
+      <Marker position={position}>
+        <Popup>Это Москва.</Popup>
+      </Marker>
+    </MapContainer>
     </div>
   );
 }
