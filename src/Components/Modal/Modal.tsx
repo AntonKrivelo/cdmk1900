970fb9e664779005be648e93f0467579b7273c1a
Anton Krivelo <krivelo2017@mail.ru>
2025-08-07 14:34:35 +0300

    remove 2

--- src/Components/Modal/Modal.tsx
+++ src/Components/Modal/Modal.tsx
@@ -3,17 +3,13 @@ import { LatLngBoundsExpression } from 'leaflet';
 const Modal = ({ bounds, onClose }: {
   bounds: LatLngBoundsExpression;
   onClose: () => void }) => {
-    const [[lat1, lng1], [lat2, lng2]] = bounds as [[number, number], [number, number]];
+    const [[lat1, lng1]] = bounds as [[number, number]];
   return (
     <div className='modal'>
       <h3>Выделенная область</h3>
       <p>
-        <strong>Координата 1:</strong><br />
+        <strong>Координаты:</strong><br />
         {lat1.toFixed(6)}, {lng1.toFixed(6)}
-      </p>
-      <p>
-        <strong>Координата 2:</strong><br />
-        {lat2.toFixed(6)}, {lng2.toFixed(6)}
       </p>
       <button onClick={onClose} style={{ marginTop: '10px' }}>
         Закрыть
