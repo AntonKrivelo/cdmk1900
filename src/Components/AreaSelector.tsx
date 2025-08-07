b2d2e7c2163f038cfc7c6211b0834a7d846fc48f
Anton Krivelo <krivelo2017@mail.ru>
2025-08-07 14:10:50 +0300

    123

--- src/Components/AreaSelector.tsx
+++ src/Components/AreaSelector.tsx
@@ -1,8 +1,7 @@
-import {useState} from 'react';
+import {useState, useRef} from 'react';
 import { Rectangle, useMapEvents } from 'react-leaflet';
-import { LatLngBoundsExpression, LatLng } from 'leaflet';
+import { LatLngBoundsExpression, LatLng, LeafletMouseEvent  } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-
 const AreaSelector = ({
   onSelect,
 }: {
@@ -10,22 +9,27 @@ const AreaSelector = ({
 }) => {
   const [start, setStart] = useState<LatLng | null>(null);
   const [end, setEnd] = useState<LatLng | null>(null);
-
-  useMapEvents({
-    mousedown(e) {
-       if (e.originalEvent) {
-      e.originalEvent.preventDefault();
-    }
+  const dragging = useRef(false);
+  const map = useMapEvents({
+    mousedown(e: LeafletMouseEvent) {
+      dragging.current = false;
       setStart(e.latlng);
       setEnd(null);
     },
-    mousemove(e) {
+    mousemove(e: LeafletMouseEvent) {
       if (start) {
-        setEnd(e.latlng);
+        // Проверим, насколько пользователь сместил мышь
+        const dx = Math.abs(e.containerPoint.x - map.latLngToContainerPoint(start).x);
+        const dy = Math.abs(e.containerPoint.y - map.latLngToContainerPoint(start).y);
+
+        if (dx > 5 || dy > 5) {
+          dragging.current = true;
+          setEnd(e.latlng);
+        }
       }
     },
-    mouseup() {
-      if (start && end) {
+    mouseup(e: LeafletMouseEvent) {
+      if (start && end && dragging.current) {
         const bounds: LatLngBoundsExpression = [
           [start.lat, start.lng],
           [end.lat, end.lng],
@@ -34,13 +38,14 @@ const AreaSelector = ({
       }
       setStart(null);
       setEnd(null);
+      dragging.current = false;
     },
     contextmenu(e) {
-    e.originalEvent.preventDefault();
-  }
+      e.originalEvent.preventDefault(); // отключаем меню браузера
+    },
   });
-  
-  return start && end ? (
+
+  return start && end && dragging.current ? (
     <Rectangle
       bounds={[
         [start.lat, start.lng],
@@ -49,8 +54,5 @@ const AreaSelector = ({
       pathOptions={{ color: 'blue' }}
     />
   ) : null;
-
-  
 };
-
 export default AreaSelector;
