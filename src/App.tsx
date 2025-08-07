25bea072f1b1e124acba56df153525c5a35d06e9
Anton Krivelo <krivelo2017@mail.ru>
2025-08-07 14:15:55 +0300

    9090

--- src/App.tsx
+++ src/App.tsx
@@ -8,29 +8,8 @@ import { LatLngBoundsExpression } from 'leaflet';
 import AreaSelector from './Components/AreaSelector';
 import './App.css';
 import 'leaflet/dist/leaflet.css';
+import Modal from './Components/Modal/Modal';
 
-
-const Modal = ({ bounds, onClose }: {
-  bounds: LatLngBoundsExpression;
-  onClose: () => void }) => {
-    const [[lat1, lng1], [lat2, lng2]] = bounds as [[number, number], [number, number]];
-  return (
-    <div className='modal'>
-      <h3>Выделенная область</h3>
-      <p>
-        <strong>Координата 1:</strong><br />
-        {lat1.toFixed(6)}, {lng1.toFixed(6)}
-      </p>
-      <p>
-        <strong>Координата 2:</strong><br />
-        {lat2.toFixed(6)}, {lng2.toFixed(6)}
-      </p>
-      <button onClick={onClose} style={{ marginTop: '10px' }}>
-        Закрыть
-      </button>
-    </div>
-  );
-};
 
 const App: React.FC = () => {
   const position: [number, number] = [53.975488, 27.411509]; 
